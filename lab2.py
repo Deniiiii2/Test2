1d6bdd22dbe903eb74762d28c312d95aef19b768
Deniiiii2 <113837843+Deniiiii2@users.noreply.github.com>
2022-09-28 11:20:52 +0500

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,23 @@
+ru = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЭЮЯ'*1000000
+eu = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'*10000000
+message=input('Введите текст ЗАГЛАВНЫМИ буквами Русского или Английского алфавита: ')
+lang=input('Введите язык RU или EU: ')
+number=int(input('Введите шаг сдвига '))
+d = ''
+if lang == 'RU':
+    for i in message:
+        place=ru.find(i)
+        new=place+number
+        if i in ru:
+            d+=ru[new]
+        else:
+            d+=i
+else:
+    for i in message:
+        place=eu.find(i)
+        new=place+number
+        if i in eu:
+            d+=eu[new]
+        else:
+            d+=i
+print(d)
